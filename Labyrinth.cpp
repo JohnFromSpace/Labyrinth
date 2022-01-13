988bf403b76d2d2e286e4ff57343cf9a3b1e3dc6
Boril <96446768+JohnFromSpace@users.noreply.github.com>
2022-01-13 22:26:08 +0200

    Update Labyrinth.cpp

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,109 +1,61 @@
-#include <time.h>
+##include <iostream>
+#include <ctime>
+#include <windows.h>
+#include <conio.h>
+#include <stack>
 #include <fstream>
+#include "stdafx.h" 
+
 using namespace std;
 
-#define GRID_WIDTH 79
-#define GRID_HEIGHT 18
-#define NORTH 0
-#define EAST 1
-#define SOUTH 2
-#define WEST 3
+#define SIZE 1501 // 79 * 19 
 
-char grid[GRID_WIDTH * GRID_HEIGHT];
+// CELL STRUCTURE
+struct Cell
+{
+    bool visited;
+    bool top_wall;
+    bool bot_wall;
+    bool left_wall;
+    bool right_wall;
+    char display;
+};
 
-void ResetGrid();
-int XYToIndex(int x, int y);
-int IsInBounds(int x, int y);
-void Visit(int x, int y);
-void PrintGrid();
+// FUNCTION DECLARATIONS
+void Initialize(Cell Level[][SIZE]);
+void ClearScreen();
+void Redraw(Cell Level[][SIZE]);
+void GenerateMaze(Cell Level[][SIZE], int& posX, int& posY, int& goalX, int& goalY);
+void SaveMaze(Cell Level[][SIZE]);
 
-int main()
-{
-	// Starting point and top-level control.
-	srand(time(0)); // seed random number generator
-	ResetGrid();
-	Visit(1, 1);
-	PrintGrid();
-	return 0;
-}
+//Main Function
+int main() {
+    Cell Level[SIZE][SIZE];
+    int posX = 0;
+    int posY = 0;
+    int goalX = 0;
+    int goalY = 0;
+    bool game_over = false;
 
-void ResetGrid()
-{
-	// Fills the grid with walls ('#' characters)
-	for (int i = 0; i < GRID_WIDTH * GRID_HEIGHT; ++i)
-	{
-		grid[i] = '#';
-	}
-}
-int XYToIndex(int x, int y)
-{
-	return y * GRID_WIDTH + x;
-}
-int IsInBounds(int x, int y)
-{
-	if (x < 0 || x >= GRID_WIDTH) return false;
-	if (y < 0 || y >= GRID_HEIGHT) return false;
-	return true;
-}
+    while (!game_over) {
+        system("cls");
+        Initialize(Level);
+        Redraw(Level);
+        GenerateMaze(Level, posX, posY, goalX, goalY);
+        SaveMaze(Level);
 
-void Visit(int x, int y)
-{
-	// Starting at the given index, recursively visits every direction in a
-	// randomized order.
-	// Set my current location to be an empty passage.
-	grid[XYToIndex(x, y)] = ' ';
-	// Create an local array containing the 4 directions and shuffle their order.
-	int dirs[4];
-	dirs[0] = NORTH;
-	dirs[1] = EAST;
-	dirs[2] = SOUTH;
-	dirs[3] = WEST;
-	for (int i = 0; i < 4; ++i)
-	{
-		int r = rand() & 3;
-		int temp = dirs[r];
-		dirs[r] = dirs[i];
-		dirs[i] = temp;
-	}
-	// Loop through every direction and attempt to Visit that direction.
-	for (int i = 0; i < 4; ++i)
-	{
-		// dx,dy are offsets from current location. Set them based
-		// on the next direction I wish to try.
-		int dx = 0, dy = 0;
-		switch (dirs[i])
-		{
-		case NORTH: dy = -1; break;
-		case SOUTH: dy = 1; break;
-		case EAST: dx = 1; break;
-		case WEST: dx = -1; break;
-		}
-		// Find the (x,y) coordinates of the grid cell 2 spots
-		// away in the given direction.
-		int x2 = x + (dx << 1);
-		int y2 = y + (dy << 1);
-		if (IsInBounds(x2, y2))
-		{
-			if (grid[XYToIndex(x2, y2)] == '#')
-			{
-				// (x2,y2) has not been visited yet... knock down the
-				// wall between my current position and that position
-				grid[XYToIndex(x2 - dx, y2 - dy)] = ' ';
-				// Recursively Visit (x2,y2)
-				Visit(x2, y2);
-			}
-		}
-	}
-}
-void PrintGrid()
-{
-	// Displays the finished maze to the screen.
-	for (int y = 0; y < GRID_HEIGHT; ++y)
-	{
-		for (int x = 0; x < GRID_WIDTH; ++x)
-		{
-			cout << grid[XYToIndex(x, y)];
-		}
-		cout << endl;
-	}
+        char input;
+        cout << "Create a new Maze? (Y)/(N): ";
+        cin >> input;
+
+        if ((input != 'n') && (input != 'N') && (input != 'y') && (input != 'Y'))
+            cout << "Invalid option" << endl;
+        else if ((input == 'n') || (input == 'N')) {
+            game_over = true;
+            cout << "Good bye!" << endl;
+        }
+    }
+
+    _getch();
+    return 0;
 }
