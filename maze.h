69bc27f1a09c0d88ebe3859052365cf350388ef3
Boril <96446768+JohnFromSpace@users.noreply.github.com>
2022-01-19 04:13:54 +0200

    Update maze.h
    After a very long time of coding ... 
    And debugging...

--- maze.h
+++ maze.h
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include "assert.h"
 using namespace std;
 
 struct Cell
@@ -29,6 +33,7 @@ struct Cell
 		west = false;
 	}
 };
+
 
 struct LinkedCell
 {
@@ -91,4 +96,57 @@ public:
 
 
 
+};
+
+//Implemented class derivative of the streambuf class
+class StreambufDoubler : public std::streambuf {
+public:
+	StreambufDoubler(std::streambuf* buf1, std::streambuf* buf2) :
+		_buf1(buf1), _buf2(buf2), _buffer(128)
+	{
+		assert(_buf1 && _buf2);
+
+		setg(0, 0, 0);
+		setp(_buffer.data(), _buffer.data(), _buffer.data() + _buffer.size());
+	}
+
+	~StreambufDoubler() {
+		sync();
+	}
+
+	void imbue(const std::locale& loc) {
+		_buf1->pubimbue(loc);
+		_buf2->pubimbue(loc);
+	}
+
+	std::streampos seekpos(std::streampos sp, std::ios_base::openmode which) {
+		return seekoff(sp, std::ios_base::cur, which);
+	}
+
+	std::streampos seekoff(std::streamoff off, std::ios_base::seekdir way, std::ios_base::openmode which) {
+		if (which | std::ios_base::in)
+			throw(std::runtime_error("Can't use this class to read data"));
+
+		_buf1->pubseekoff(off, way, which);
+		return _buf2->pubseekoff(off, way, which);
+	}
+
+	int overflow(int c) {
+		int retValue = sync() ? EOF : 0;
+		sputc(c);
+		return retValue;
+	}
+
+	int sync() {
+		_buf1->sputn(pbase(), pptr() - pbase());
+		_buf2->sputn(pbase(), pptr() - pbase());
+		setp(_buffer.data(), _buffer.data(), _buffer.data() + _buffer.size());
+		return _buf1->pubsync() | _buf2->pubsync();
+	}
+
+private:
+	std::streambuf* _buf1;
+	std::streambuf* _buf2;
+
+	std::vector<char>   _buffer;
 };
