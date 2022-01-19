48d813a606a1aed8702802510dc6a0cff62623a7
Boril <96446768+JohnFromSpace@users.noreply.github.com>
2022-01-20 00:35:02 +0200

    Added an if-statement and PRODUCT_LIMIT

--- maze-generator.cpp
+++ maze-generator.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include "stdafx.h"
 
+const int PRODUCT_LIMIT = 1600;
+
 int main() {
 
 	rlutil::setColor(15);//Sets color to white
@@ -22,70 +24,77 @@ int main() {
 
 	cin >> sizeX >> sizeY;
 	
-	cout << "\n\n";
-
-	cout << "A maze with dimensions: " <<
-		"width = " << sizeX << " and height = " << sizeY << endl;
-
-	Maze maze(sizeX, sizeY);
-	maze.generateMaze();
-	maze.printMaze(false);
-
-	cout << "\n\n";
-	cout << "THE MAZE WAS SAVED IN A .TXT FILE.\nEven if the maze is 80 x 20 you should not have problem" 
-		<<" with zooming out on Notepad.\n\n";
+	//The PRODUCT_LIMIT should NOT be equal or more than 1600!!!
+	if (sizeX * sizeY < PRODUCT_LIMIT) {
 	
-	myMaze.close();
+		cout << "\n\n";
 
+		cout << "A maze with dimensions: " <<
+			"width = " << sizeX << " and height = " << sizeY << endl;
 
-	int entranceX, entranceY, exitX, exitY;
-	cout << endl << "Enter the entrance coordinates (X: 0-" << sizeX - 1
-		<< " & Y: 0-" << sizeY - 1 << "): ";
-	cin >> entranceX >> entranceY;
-	maze.setEntrance(entranceX, entranceY);
-
-	cout << "Enter the exit coordinates (X: 0-" << sizeX - 1
-		<< " & Y: 0-" << sizeY - 1 << "): ";
-	cin >> exitX >> exitY;
-	maze.setExit(exitX, exitY);
-
-	cout << "\n\n";
-
-	myMaze.open("Maze.txt", std::ios_base::app); // append instead of overwrite
-
-	myMaze << "Starting point: (" << entranceX << ", " << entranceY << ")\n";
-	myMaze << "Exit point: (" << exitX << ", " << exitY << ")\n\n\n";
-
-	myMaze.close();
-
-	myMaze << "Starting point: (" << entranceX << ", " << entranceY << ")\n";
-	myMaze << "Exit point: (" << exitX << ", " << exitY << ")\n\n\n";
-
-	cout << "Do you want the program to solve it for you? ('Y', 'y', 'N' or 'n')\n";
-
-	char input;
-	cin >> input;
-
-	if (input == 'Y' || input == 'y') {
-
-		rlutil::setColor(8);//Sets color to dark grey
-
-		std::ofstream solutionFile("Solution.txt");
-		StreambufDoubler doublerS(std::cout.rdbuf(), solutionFile.rdbuf());
-		std::cout.rdbuf(&doublerS);
-
-		maze.solveMaze();
-		maze.printMaze(true);
-
-		solutionFile.close();
-
-		rlutil::setColor(15);//Sets color back to normal
-
-		solutionFile.close();
+		Maze maze(sizeX, sizeY);
+		maze.generateMaze();
+		maze.printMaze(false);
 
 		cout << "\n\n";
-		cout << "THE SOLUTION WAS SAVED IN A .TXT FILE.\n\n";
-	}
+		cout << "THE MAZE WAS SAVED IN A .TXT FILE.\nEven if the maze is 80 x 20 you should not have problem" 
+			<<" with zooming out on Notepad.\n\n";
 
+		myMaze.close();
+
+
+		int entranceX, entranceY, exitX, exitY;
+		cout << endl << "Enter the entrance coordinates (X: 0-" << sizeX - 1
+			<< " & Y: 0-" << sizeY - 1 << "): ";
+		cin >> entranceX >> entranceY;
+		maze.setEntrance(entranceX, entranceY);
+
+		cout << "Enter the exit coordinates (X: 0-" << sizeX - 1
+			<< " & Y: 0-" << sizeY - 1 << "): ";
+		cin >> exitX >> exitY;
+		maze.setExit(exitX, exitY);
+
+		cout << "\n\n";
+
+		myMaze.open("Maze.txt", std::ios_base::app); // append instead of overwrite
+
+		myMaze << "Starting point: (" << entranceX << ", " << entranceY << ")\n";
+		myMaze << "Exit point: (" << exitX << ", " << exitY << ")\n\n\n";
+
+		myMaze.close();
+
+		myMaze << "Starting point: (" << entranceX << ", " << entranceY << ")\n";
+		myMaze << "Exit point: (" << exitX << ", " << exitY << ")\n\n\n";
+
+		cout << "Do you want the program to solve it for you? ('Y', 'y', 'N' or 'n')\n";
+
+		char input;
+		cin >> input;
+
+		if (input == 'Y' || input == 'y') {
+
+			rlutil::setColor(8);//Sets color to dark grey
+
+			std::ofstream solutionFile("Solution.txt");
+			StreambufDoubler doublerS(std::cout.rdbuf(), solutionFile.rdbuf());
+			std::cout.rdbuf(&doublerS);
+
+			maze.solveMaze();
+			maze.printMaze(true);
+
+			solutionFile.close();
+
+			rlutil::setColor(15);//Sets color back to normal
+
+			solutionFile.close();
+
+			cout << "\n\n";
+			cout << "THE SOLUTION WAS SAVED IN A .TXT FILE.\n\n";
+		}
+	}
+	
+	else{
+		return -1;
+	}
 	return 0;
 }
